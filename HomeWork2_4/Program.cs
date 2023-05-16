b104b31a4ba4da1deed6d0e60d70ae8d39388860
nniew <122811326+nniew@users.noreply.github.com>
2023-05-16 23:41:56 +0700

    Add files via upload

---
+++ HomeWork2_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+  public static void Main (string[] args) {
+    int n, k;
+    String num = Console.ReadLine();
+    String[] numsp = num.Split(' ');
+    int.TryParse(numsp[0], out n);
+    int.TryParse(numsp[1], out k);
+
+    String population = Console.ReadLine();
+    String[] populationsp = population.Split(' ');
+    
+    int[] p = new int[n];
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++){
+        int.TryParse(populationsp[i], out p[i]);
+    }
+    int total = 0;
+    for(int i = k; i < n-k; i++){
+      total = 0;
+      for(int j = 1; j <= k; j++){
+        total = total + p[i-j] + p[i] + p[i+j];
+        }
+      result[i] = total;
+    }
+    Console.WriteLine(result.Max());
+  }
+}
